8e44675379fb45bb72a23d5429ac22ef68274f0b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-15 18:03:18 +0530

    Done for today

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -25,12 +25,12 @@ const page = () => {
             No 1 Task Management
           </div>
           <h1 className='text-3xl md:text-6xl text-center text-neutral-800 mb-6'>Taskilio helps teams move</h1>
-          <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">work forward.</div>
+          <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md w-fit">work forward.</div>
         </div>
 
     <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center", textFont.className)}>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique- accomplish it all with Taskilio.
         </div>
-        <Button className='mt-6' size={'lg'} asChild>
+        <Button className={cn('mt-6', textFont.className)} size={'lg'} asChild>
           <Link href={'/sign-up'}>Get Started for Free</Link>
         </Button>
       </div>
