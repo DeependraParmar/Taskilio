f063665d035efc465218793fed46b96d06db6b2d
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-18 05:20:24 +0530

    Added ClerkProvider and middleware.ts

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,9 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+const PlatformLayout = ({children}: {children: React.ReactNode}) => {
+    <ClerkProvider>
+        {children}
+    </ClerkProvider>
+}
+
+export default PlatformLayout;
